c7db8cd84f1bf0c1d50d712c768c61e416719c9a
root-lucas <945832032@qq.com>
2020-09-06 22:17:40 +0800

    feat: confirm Order

--- src/router/index.js
+++ src/router/index.js
@@ -36,6 +36,14 @@ const routes = [
         },
     },
     {
+        path: '/confirmOrder',
+        name: 'ConfirmOrder',
+        component: () => import('../views/ConfirmOrder.vue'),
+        meta: {
+            requireAuth: true, // 需要验证登录状态
+        },
+    },
+    {
         path: '/error',
         name: 'Error',
         component: () => import('../components/Error.vue'),
